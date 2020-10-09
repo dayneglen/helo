e4f24dbf414ce7a2dc90f629f81ccc6e14ff0ed0
dayneglen <dayneglen@gmail.com>
2020-10-09 14:00:45 -0600

    updated dashboard layout and styling

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './Dashboard.css';
 
 class Dashboard extends Component {
     constructor() {
@@ -17,10 +18,27 @@ class Dashboard extends Component {
         }
     }
 
-    render() {
-        return (
-            <div className = 'Dashboard'>
+    handleChange = e => {
+        this.setState({[e.target.name]: e.target.value})
+    }
 
+    render() {
+        console.log(this.state.search)
+        return (
+            <div className='Dashboard'>
+                <section className='dashboard-container'>
+                    <section className='search-posts'>
+                        <section className='search-input'>
+                            <input value={this.state.search} name='search' placeholder='Search by Title' onChange={this.handleChange} />
+                            <button>Search</button>
+                            <button>Reset</button>
+                        </section>
+                        <section className='posts-checked'>
+                            <h3>My Posts</h3>
+                            <input type='checkbox' />
+                        </section>
+                    </section>
+                </section> 
             </div>
         )
     }
